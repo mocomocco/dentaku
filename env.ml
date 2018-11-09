e7f34b2049e0c1a06714dbb84d7975683ba6cca4
大洞　日音 <HINARI@oohoranichion-no-MacBook-Air.local>
2018-11-09 13:51:19 +0900

    envをlistにした

--- env.ml
+++ env.ml
@@ -1,27 +1,15 @@
-	(* キーが 'a 型、値が 'b 型の環境を表す型 *)
-type ('a, 'b) t = Empty
-                  | Node of {
-                              key : 'a ;
-                              value : 'b ;
-                              next : ('a , 'b) t ;
-                          }
-(*(String*Value.t) list*)
-let empty=Empty
+type ('a, 'b) t = ('a * 'b) list
 
-let rec get kankyou hensuu = match kankyou with
-  Empty ->raise Not_found
- |Node {key=namae;value=atai;next=tsugi} ->
-     if namae = hensuu then
-        atai
-     else
-        get tsugi hensuu
+let empty=[]
+
+let get kankyou hensuu = List.assoc hensuu kankyou
 
 (*val get : ('a, 'b) t -> 'a -> 'b*)
 	(* 使い方：get env var *)
 	(* 環境 env の中で変数 var の値を返す *)
 	(* 変数 var が見つからなかったら例外 Not_found を起こす *)
 
-let extend kankyou hensuumei atai = Node{key=hensuumei;value=atai;next=kankyou}
+let extend kankyou hensuumei atai = (hensuumei,atai)::kankyou
 
 (*val extend : ('a, 'b) t -> 'a -> 'b -> ('a, 'b) t*)
 	(* 使い方：extend env var value *)
