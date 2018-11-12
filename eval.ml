30c004b743d6c4e8b1e756429a49cd27533cae6d
大洞　日音 <HINARI@oohoranichion-no-MacBook-Air.local>
2018-11-12 14:32:11 +0900

    1112

--- eval.ml
+++ eval.ml
@@ -84,28 +84,13 @@ let rec f expr kankyou=
   | If (arg1, arg2, arg3) ->(*if arg1 then arg2 else arg3*)
       let v1 = f arg1 kankyou in
       begin match v1 with
-        VBool(true) -> let v2 =f arg2 kankyou in
-                  (match v2 with
-                   VBool(b2)->VBool(b2)
-                   |VNumber(n2)->VNumber(n2)
-                   |Vclo(x,t,env)->Vclo(x,t,env)
-                 )
-       |VBool(false) -> let v3=f arg3 kankyou in
-                  (match v3 with
-                   VBool(b3)->VBool(b3)
-                   |VNumber(n3)->VNumber(n3)
-                   |Vclo(x,t,env)->Vclo(x,t,env)
-                 )
+        VBool(true) -> f arg2 kankyou
+       |VBool(false) -> f arg3 kankyou
        | _ ->failwith("Predicate part is not a boolean: "^ Value.to_string v1)
     end
   | Let (arg1, arg2, arg3) ->(*let arg1 = arg2 in arg3 ex: let x=3 in x+2*)
       let atai= f arg2 kankyou in
         f arg3 (set_value kankyou arg1 atai)
-(*      begin match atai with
-        VNumber(n1)-> f arg3 (set_value kankyou arg1 atai)
-      | VBool(b1) -> failwith("boolは無理")
-      | Vclo(x,t,env) -> f arg3 (set_value kankyou arg1 atai)
-      end*)
   | Fun (arg1, arg2) ->(*fun arg1:string -> arg2:expr  ex: fun x ->x+1*)
       Vclo(arg1,arg2,kankyou)
   | App (arg1, arg2) ->(*arg1 arg2*)
@@ -113,9 +98,5 @@ let rec f expr kankyou=
       let hikisuu=f arg2 kankyou in
       begin match kansuu with
         Vclo(x,t,env) -> f t (set_value env x hikisuu)
-          (*(match hikisuu with
-             VNumber(n1) -> f t (set_value env x hikisuu)
-            |VBool(b1) -> f t (set_value env x hikisuu)
-            | _ -> failwith("Argument is <fun>"))*)
        |_ ->failwith(to_string kansuu ^ " is not function")
       end
